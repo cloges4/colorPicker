82f5b2e2ae4962086efcf83a1de29be9646badf0
cloges4 <c4webdev@gmail.com>
2019-03-11 21:12:55 -0400

    add script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+/* 
+- on load random generate bg color 
+  - display hex value on input
+- click spacebar random gen bg color
+  - display hex value on input
+- on type bg changes to hex value starting at 3 hex
+
+
+ - if bg color is dark text color needs to be a white color
+  - if bg color is light text color needs to be black color
+*/
+
+// var input = document.getElementById("hex").value;
+
+// console.log(input);
