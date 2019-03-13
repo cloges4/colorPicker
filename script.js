4c162ae6444a40266c3e504ba39711ddb5581066
cloges4 <c4webdev@gmail.com>
2019-03-13 19:48:54 -0400

    created random number generator function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 - on load random generate bg color 
   - display hex value on input
 - click spacebar random gen bg color
-  - display hex value on input
+  DONE - display hex value on input
 - on type bg changes to hex value starting at 3 hex
 
 
@@ -12,7 +12,22 @@
 
 var input = document.getElementById("hex");
 var body = document.getElementsByTagName("body")[0];
+var numberArray = [];
 
 input.addEventListener("input", function() {
   body.style.background = "#" + input.value;
 });
+
+// random number between 1 and 16
+function randomHex() {
+  for (var i = 0; i < 6; i++) {
+    var random = Math.floor(Math.random() * (16 - 1)) + 1;
+    numberArray.push(random);
+  }
+  return numberArray;
+  // var hexValue = hexArray.join("");
+}
+
+randomHex();
+
+// console.log(numberArray);
