ad963191660be55c5017109b93954253700bc15f
cloges4 <c4webdev@gmail.com>
2019-03-14 21:53:47 -0400

    random number generator returns hex value and added event listener to keypress

--- script.js
+++ script.js
@@ -12,22 +12,44 @@
 
 var input = document.getElementById("hex");
 var body = document.getElementsByTagName("body")[0];
-var numberArray = [];
 
 input.addEventListener("input", function() {
   body.style.background = "#" + input.value;
 });
 
 // random number between 1 and 16
-function randomHex() {
+function hexGenerator() {
+  var numberArray = [];
   for (var i = 0; i < 6; i++) {
-    var random = Math.floor(Math.random() * (16 - 1)) + 1;
+    var random = Math.floor(Math.random() * 16);
+    if (random === 10) {
+      random = "A";
+    } else if (random === 11) {
+      random = "B";
+    } else if (random === 12) {
+      random = "C";
+    } else if (random === 13) {
+      random = "D";
+    } else if (random === 14) {
+      random = "E";
+    } else if (random === 15) {
+      random = "F";
+    }
     numberArray.push(random);
+    var hexValue = numberArray.join("");
   }
-  return numberArray;
+  return hexValue;
   // var hexValue = hexArray.join("");
 }
 
-randomHex();
-
+// hexGenerator();
 // console.log(numberArray);
+
+var arrayValues = hexGenerator();
+console.log(arrayValues);
+
+addEventListener("keypress", function() {
+  if (event.keyCode === 13 || event.keyCode === 32) {
+    body.style.background = "#" + arrayValues;
+  }
+});
