ac8908704b3b14384e88f286926a19d9d7bc7f85
cloges4 <c4webdev@gmail.com>
2019-07-10 20:56:11 -0400

    removed loop for hexgen function added forEach

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ input.addEventListener("input", function() {
 
 // random number between 0 and 15
 function hexGenerator() {
-  let numberArray = [];
-  for (let i = 0; i < 6; i++) {
+  let numberArray = ["", "", "", "", "", ""];
+  numberArray.forEach(function() {
     let random = Math.floor(Math.random() * 16);
     switch (random) {
       case 10:
@@ -33,22 +33,9 @@ function hexGenerator() {
         break;
       default:
     }
-    // if (random === 10) {
-    //   random = "A";
-    // } else if (random === 11) {
-    //   random = "B";
-    // } else if (random === 12) {
-    //   random = "C";
-    // } else if (random === 13) {
-    //   random = "D";
-    // } else if (random === 14) {
-    //   random = "E";
-    // } else if (random === 15) {
-    //   random = "F";
-    // }
     numberArray.push(random);
     hexValue = numberArray.join("");
-  }
+  });
   return hexValue;
 }
 //spacebar or enter key takes random hex value to bg and output to the input
