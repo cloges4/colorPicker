46572c918c6f91b8afb696338cfd1c27ba94b817
cloges4 <c4webdev@gmail.com>
2019-07-10 20:41:05 -0400

    replaced if statements in hexGen function with switch

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var input = document.getElementById("hex");
-var body = document.getElementsByTagName("body")[0];
-var hexValue;
+let input = document.getElementById("hex");
+let body = document.getElementsByTagName("body")[0];
+let hexValue;
 
 // input value to bg color
 input.addEventListener("input", function() {
@@ -9,22 +9,43 @@ input.addEventListener("input", function() {
 
 // random number between 0 and 15
 function hexGenerator() {
-  var numberArray = [];
-  for (var i = 0; i < 6; i++) {
-    var random = Math.floor(Math.random() * 16);
-    if (random === 10) {
-      random = "A";
-    } else if (random === 11) {
-      random = "B";
-    } else if (random === 12) {
-      random = "C";
-    } else if (random === 13) {
-      random = "D";
-    } else if (random === 14) {
-      random = "E";
-    } else if (random === 15) {
-      random = "F";
+  let numberArray = [];
+  for (let i = 0; i < 6; i++) {
+    let random = Math.floor(Math.random() * 16);
+    switch (random) {
+      case 10:
+        random = "A";
+        break;
+      case 11:
+        random = "B";
+        break;
+      case 12:
+        random = "C";
+        break;
+      case 13:
+        random = "D";
+        break;
+      case 14:
+        random = "E";
+        break;
+      case 15:
+        random = "F";
+        break;
+      default:
     }
+    // if (random === 10) {
+    //   random = "A";
+    // } else if (random === 11) {
+    //   random = "B";
+    // } else if (random === 12) {
+    //   random = "C";
+    // } else if (random === 13) {
+    //   random = "D";
+    // } else if (random === 14) {
+    //   random = "E";
+    // } else if (random === 15) {
+    //   random = "F";
+    // }
     numberArray.push(random);
     hexValue = numberArray.join("");
   }
